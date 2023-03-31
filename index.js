b8a1483cc54169e42f668416120340c739b08b49
Rakib01754 <rakibul.freelancer567@gmail.com>
2023-03-31 23:38:09 +0600

    some get operation done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
@@ -15,7 +15,61 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n58ahyf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri)
+
+async function run() {
+    try {
+        // databse collections 
+        const productCollection = client.db("OurShop").collection("products");
+        const brandCollection = client.db("OurShop").collection("brand");
+        // get products from database
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        });
+        // get limited data for homepage 
+        app.get('/productssample', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query).limit(5)
+            const productsData = await cursor.toArray()
+            res.send(productsData)
+        });
+
+        //get brands from database
+        app.get('/brands', async (req, res) => {
+            const query = {}
+            const cursor = brandCollection.find(query).limit(5)
+            const brands = await cursor.toArray()
+            res.send(brands)
+        });
+
+        //get advertised product by query
+        app.get('/advertised', async (req, res) => {
+            const query = { advertise: 'true' }
+            const data = await productCollection.find(query).toArray();
+            res.send(data)
+        });
+
+        //get specific products by id
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await productCollection.findOne(query);
+            res.send(result)
+        })
+        //get specific products by id
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { categoryId: id }
+            const result = await productCollection.find(query).toArray();
+            res.send(result)
+        })
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
